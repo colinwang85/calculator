7c8a426818201e74287be762303be6dfcc32fe99
colinwang85@163.com <colinwang85@163.com>
2018-09-26 09:56:28 +0800

    enhancement

--- src/main/java/com/calculator/services/impl/SqrtOperatorServiceImpl.java
+++ src/main/java/com/calculator/services/impl/SqrtOperatorServiceImpl.java
@@ -23,7 +23,6 @@ public class SqrtOperatorServiceImpl implements OperatorService {
     @Override
     public Operation operator(Stack<BigDecimal> paraStack) {
         BigDecimal a = paraStack.pop();
-        Double result = Math.sqrt(a.doubleValue());
         paraStack.push(NumberFormatUtil.bigDecimalScaleFormat(Math.sqrt(a.doubleValue())));
         return new Operation(paraStack.size(), this, new BigDecimal[]{a});
     }
